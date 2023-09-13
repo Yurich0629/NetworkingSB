3e8df9f66ab30ed254f6766af2bc7d78a753542c
Ilya Desiatow <140006151+Yurich0629@users.noreply.github.com>
2023-09-13 16:16:28 +0300

    Initial Commit

---
+++ NetworkingSB/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NetworkingSB
+//
+//  Created by Илья Десятов on 13.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
